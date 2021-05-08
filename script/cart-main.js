0b7e1380ba7aa57f749e7dad6ddda7d953eb0db5
joojeonghyeon99 <jjoo@skedulo.com>
2021-05-08 23:14:52 +0700

    almost done

--- script/cart-main.js
+++ script/cart-main.js
@@ -5,21 +5,26 @@ const products = [
   {
     name: "LINEN WORKER JACKET",
     price: 100.99,
+    tag: "jacket1",
     quantity: 0,
   },
   {
     name: "BEIGE LINEN JACKET",
     price: 129.99,
+    tag: "jacket2",
+
     quantity: 0,
   },
   {
     name: "BLACK RIDER JACKET",
     price: 250.99,
+    tag: "jacket3",
     quantity: 0,
   },
   {
     name: "BLACK WORKER JACKET",
     price: 129.99,
+    tag: "jacket4",
     quantity: 0,
   },
 ];
@@ -27,10 +32,12 @@ const products = [
 carts.forEach((cart, index) =>
   cart.addEventListener("click", () => {
     cartNumbers(products[index]);
+    totalCost(products[index]);
   })
 );
 
-function cartNumbers() {
+function cartNumbers(product) {
+  console.log("product :>> ", product);
   let cartTotalNumbers = parseInt(localStorage.getItem("cartTotalNumbers"));
 
   cartTotalNumbers
@@ -38,8 +45,33 @@ function cartNumbers() {
       (document.getElementById("basket-number").textContent = cartTotalNumbers))
     : localStorage.setItem("cartTotalNumbers", 1);
 
+  setItems(product);
+
   console.log("cartTotalNumbers :>> ", cartTotalNumbers);
 }
+
+const setItems = (product) => {
+  let { name, price, quantity, tag } = product;
+
+  let cartItems = localStorage.getItem("productsInCart");
+
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    }
+  } else {
+    (product.quantity = 1), (cartItems = { [product.tag]: product });
+  }
+
+  console.log("cartsItem :>> ", cartItems);
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+};
 
 const cartNumberOnload = () => {
   let cartTotalNumbers = parseInt(localStorage.getItem("cartTotalNumbers"));
@@ -50,4 +82,37 @@ const cartNumberOnload = () => {
     : (document.getElementById("basket-number").textContent = 0);
 };
 
+const totalCost = (product) => {
+  let total = localStorage.getItem("totalCost");
+
+  console.log("parseInt(total) :>> ", typeof parseFloat(total));
+
+  if (total != null) {
+    total = parseFloat(total);
+    localStorage.setItem("totalCost", total + product.price);
+    console.log("total :>> ", total);
+  } else {
+    localStorage.setItem("totalCost", product.price);
+  }
+};
+
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart")
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector
+    (".products");
+    let total = localStorage.getItem("totalCost");
+
+    console.log(cartItems);
+    if(cartItems && productContainer){
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(Item=>{
+            productContainer.innerHTML += '<div class="product"><ion-icon name="close-circle"></ion-icon><img src="../storepages/images/Jacket/${item.tag}.jpeg"><span>${item.name}</span></div><div class="price">$${item.price},00</div><div class="quantity"><ion-icon class="decrease" name="arrow-dropleft-circle"></ion-icon><span>${item.inCart}</span><ion-icon class="increase" name="arrow-dropright-circle"></ion-icon></div><div class="total">$${item.inCart * item.price},00</div>'
+        });
+        productContainerinnerHTMl +='<div class="basketTotalContainer"><h4 class="basketTotal">Basket Total</h4><h4 class"basketTotal">$${cartCost},00</h4>'
+
+    }
+}
+
 cartNumberOnload();
+displayCart();
